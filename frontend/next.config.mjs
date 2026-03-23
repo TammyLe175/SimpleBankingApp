021cc7bb8f47dd395bcad1915a62ca72d15ffd9b
TammyLe175 <129108593+TammyLe175@users.noreply.github.com>
2026-03-23 15:16:25 -0500

    Add backend URL to the frontend - frontend is hook up with backend endpoints

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  async rewrites() {
-    const backendBaseUrl = process.env.BACKEND_API_BASE_URL ?? "http://localhost:5226";
-
-    return [
-      {
-        source: "/api-proxy/:path*",
-        destination: `${backendBaseUrl}/:path*`,
-      },
-    ];
-  },
+  output: 'export',
 };
 
 export default nextConfig;
